0841dd98d2b3772227d148ecb1bc1555aac27f41
illiCristian <svrk73@gmail.com>
2023-05-17 19:05:45 -0300

    correcciones en desafio 7

--- Desafio-7/src/Dao/routes/productsDb.routes.js
+++ Desafio-7/src/Dao/routes/productsDb.routes.js
@@ -1,31 +1,61 @@
 import { Router } from "express";
 import productModel from "../models/products.js";
-
+import ProductManagerDb from "../Manager/productManagerDb.js";
 const router = Router();
-
+const productManager = new ProductManagerDb();
+/* test: http://localhost:8080/api/productsDatabase/
+result: {
+  "docs": [
+    {...}
+  ],
+  "totalDocs": 50,
+  "limit": 20,
+  "totalPages": 3,
+  "page": 1,
+  "pagingCounter": 1,
+  "hasPrevPage": false,
+  "hasNextPage": true,
+  "prevPage": null,
+  "nextPage": 2
+}*/
 router.get("/", async (req, res) => {
+  const { page = 1, limit, category, sort } = req.query;
+  const options = { page, limit: parseInt(limit) || 20, lean: true };
+  if (sort) {
+    options.sort = { [sort]: 1 };
+  } else {
+    options.sort = { title: 1 };
+  }
   try {
-    const result = await productModel.find();
+    const result = await productManager.getAllProducts(category, options);
     res.status(200).json(result);
   } catch (error) {
-    console.log(error);
+    res.status(500).send({ message: error.message });
   }
 });
+
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await productModel.findById(id);
+    const result = await productManager.getProductById(id);
     res.status(200).json(result);
   } catch (error) {
-    console.log(error);
+    res.status(500).send({ message: error.message });
   }
 });
 
 router.post("/", async (req, res) => {
   try {
-    const { title, description, price, stock, thumbnail, code, category } =
-      req.body;
-    console.log(title, description, price, stock, thumbnail, code, category);
+    const {
+      title,
+      description,
+      price,
+      stock,
+      thumbnail,
+      code,
+      category,
+      status,
+    } = req.body;
     if (
       !title ||
       !description ||
@@ -45,16 +75,17 @@ router.post("/", async (req, res) => {
       thumbnail,
       code,
       category,
+      status,
     };
 
-    const result = await productModel.create(product);
+    const result = await productManager.createProduct(product);
     res.status(201).json(result);
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
 });
 //http://localhost:8080/api/productsDatabase/6462956ccdb11f7f26a847e0
-//req.body 
+//req.body
 //result status: false
 router.put("/:id", async (req, res) => {
   try {
@@ -88,8 +119,8 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await productModel.findByIdAndDelete(id);
-    res.status(200).json(result);
+    const res = await productManager.deleteProductId(id);
+    res.status(200).json(res);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error al eliminar" });
